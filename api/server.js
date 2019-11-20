cfdd76440df84c55bc79711b618f7719974b1610
kly001 <karenly1961@gmail.com>
2019-11-19 21:50:54 -0500

    car displayed

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const server = express();
 
-// const carsRouter = require('../cars/cars-router.js')
+const carsRouter = require('../cars/cars-router.js')
 
 server.use(express.json());
-// server.use('/api/cars', carsRouter);
+server.use('/api/cars', carsRouter);
 
 server.get('/',(req,res) => {
     res.send("<h1>webdb-ii-challenge</h1>")
